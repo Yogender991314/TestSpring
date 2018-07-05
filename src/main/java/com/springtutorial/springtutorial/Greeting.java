4d7aeb73580e9b32b0fbbbda5140002066e46c66
Yogender991314 <40838197+Yogender991314@users.noreply.github.com>
2018-07-05 12:17:28 +0530

    Add files via upload

---
+++ src/main/java/com/springtutorial/springtutorial/Greeting.java
@@ -0,0 +1,24 @@
+package com.springtutorial.springtutorial;
+
+public class Greeting {
+
+    private String name;
+
+    public Greeting(String name) {
+        this.name = name;
+
+    }
+
+    public Greeting() {
+
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+}
